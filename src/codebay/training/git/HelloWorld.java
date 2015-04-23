568466e8865aafdc4ec59d7c3e52d32b25a7acc1
jralonso84 <jralonso84@gmail.com>
2015-04-23 19:50:06 +0100

    Update HelloWorld.java

--- src/codebay/training/git/HelloWorld.java
+++ src/codebay/training/git/HelloWorld.java
@@ -3,7 +3,7 @@ package codebay.training.git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World from GIT! Awesome9");
+		System.out.println("Hello World from GIT!");
 	}
 
 }
